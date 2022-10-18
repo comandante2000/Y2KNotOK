9169ea9bb2602355ab760e5032e4df63a855135d
comandante2000 <joeycomandante19@gmail.com>
2022-10-18 13:20:18 +0800

    background color

--- src/app/pages/Device/Device.jsx
+++ src/app/pages/Device/Device.jsx
@@ -3,6 +3,7 @@ import Screen from "../Screen/Screen";
 import Buttons from "../Buttons/Buttons";
 import Header from "../Header/Header";
 export default function Device() {
+  
   return (
     <div id="pda-device">
       <div className="container v2 d-flex justify-content-center gx-0 gy-0">
