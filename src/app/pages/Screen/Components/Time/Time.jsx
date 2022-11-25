1ccf26c110c5008bbe267553591756bbf8f0544a
Louie Glenn Dalida <louieglenndalida@gmail.com>
2022-11-25 20:32:02 +0800

    resize header

--- src/app/pages/Screen/Components/Time/Time.jsx
+++ src/app/pages/Screen/Components/Time/Time.jsx
@@ -2,15 +2,15 @@ import "./Time.scss";
 
 export default function Time() {
   return (
-<div  id="time-component">
-    <div className=" screen-head d-flex bd-highlight">
-      <div className="bar"></div>
-      <div className="battery"></div>
-      <div className="time">
-        <div className="d-flex justify-content-center clock">Monday 10:AM</div>
-      </div> 
-    </div>
-      <div className="line"></div>
+      <div  id="time-component">
+        <div className=" screen-head d-flex bd-highlight justify-content-sm-center">
+          <div className="bar"></div>
+          <div className="battery"></div>
+          <div className="time">
+            <div className="d-flex justify-content-center clock">Monday 10:AM</div>
+          </div>
+        </div>
+        <div className="line"></div>
       </div>
   );
 }
