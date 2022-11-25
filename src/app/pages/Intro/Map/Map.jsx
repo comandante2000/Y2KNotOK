e4cf9b1f1a3bedb78aa744067f71833dd94f8478
comandante2000 <joeycomandante19@gmail.com>
2022-11-25 09:40:42 +0800

    map zoom in

--- src/app/pages/Intro/Map/Map.jsx
+++ src/app/pages/Intro/Map/Map.jsx
@@ -1,27 +1,31 @@
-
 import "./Map.scss";
 import Time from "../../Screen/Components/Time/Time";
 import Header from "../../Screen/Components/Headers/Header";
 import MapPageOne from "./pages/Map1/MapPageOne";
+import MapPageTwo from "./pages/MapPageTwo/MapPageTwo";
 import MapLocatingPage from "./pages/Locating/Locating";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 export default function MapPage() {
 
+  const [zoom, setZoom] = useState(true);
 
-    return (
-      <div id="pda-map-screen">
-        <div className=" row-2 d-flex justify-content-center">
-          <div className="inner-row-left"></div>
-          <div className="inner-row-center">
+  console.log("the zoom", zoom);
+
+ console.log("ttt",typeof setZoom);
+
+  return (
+    <div id="pda-map-screen">
+      <div className=" row-2 d-flex justify-content-center">
+        <div className="inner-row-left"></div>
+        <div className="inner-row-center">
           <Time />
-            <Header name="Map"/>
-            <MapLocatingPage/>
-            {/* <MapPageOne/> */}
-
-          </div>
-          <div className="inner-row-right"></div>
+          <Header name="Map" />
+          {/* <MapLocatingPage/> */}
+       
+          {zoom ?<MapPageTwo setZoom={setZoom}/>:<MapPageOne setZoom={setZoom}/>}
         </div>
+        <div className="inner-row-right"></div>
       </div>
-    );
-  }
-
+    </div>
+  );
+}
---
+++ src/app/pages/Intro/Map/pages/MapPageTwo/MapPageTwo.jsx
@@ -0,0 +1,29 @@
+import "./MapPageTwo.scss";
+import { useState } from "react";
+export default function MapPageTwo({setZoom}) {
+console.log("fttt",typeof setZoom);
+
+  const Zoom = () => {
+setZoom(false);
+  
+  };
+  return (
+    <div id="map-pagetwo-screen">
+      <div className=" image d-flex justify-content-center">
+        <div className="Mappagetwo position-relative">
+          <div className="d-flex justify-content-center">
+            <div className="image-popup">
+              <div class="centered"></div>
+            </div>
+          </div>
+
+          <div
+            className="zoomout position-absolute bottom-0 start-0"
+            type="button"
+            onClick={Zoom}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+}
