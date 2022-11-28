ad008e9bb7ac6878dba6da328c2c21eb11938e9c
Louie Glenn Dalida <louieglenndalida@gmail.com>
2022-11-28 20:59:11 +0800

    fixed voicemail resizing

--- src/app/pages/Intro/VoiceMail/VoiceMailPage.jsx
+++ src/app/pages/Intro/VoiceMail/VoiceMailPage.jsx
@@ -18,98 +18,102 @@ export default function VoiceMailPage() {
           <Time />
           <Header name="VoiceMail" />
 
-          <div className=" d-flex justify-content-evenly mx-1 me-3 border border-dark py-2 g-5">
-            <div className="red-icon "></div>
-            <div className="voice-icon "></div>
-            <div className="d-flex align-items-start flex-column  justify-content-evenly">
-              <div className="d-flex justify-content-center">
-                <div className="email-name">Timothee</div>
-                <div className="ms-5">12/30</div>
+          <div className=" d-flex border border-dark py-0 voice-mail-container">
+            <div className="icon-container d-flex align-items-center">
+              <div className="red-icon "></div>
+              <div className="voice-icon "></div>
+            </div>
+            <div className="info-container me-3 m-auto">
+              <div className="d-flex w-100">
+                <div className="email-name w-50">Timothee</div>
+                <div className="voice-date w-50 text-end">12/30</div>
               </div>
-              <div className="d-flex justify-content-center">
-                <div
-                  className="play-icon"
-                  type="button"
-                  onClick={OpenMessage}
-                ></div>
-                <div className="sound-icon mt-1"></div>
+              <div className="d-flex w-100 align-items-center">
+                <div className="play-icon" type="button" onClick={OpenMessage}></div>
+                <div className="sound-icon"></div>
               </div>
             </div>
           </div>
 
-          <div className=" d-flex justify-content-evenly mx-1 me-3 border border-dark py-2 g-5 border-top-0">
-            <div className="red-icon "></div>
-            <div className="voice-icon "></div>
-            <div className="d-flex align-items-start flex-column  justify-content-evenly">
-              <div className="d-flex justify-content-center">
-                <div className="email-name">Timothee</div>
-                <div className="ms-5">12/30</div>
+          <div className=" d-flex border border-dark py-0 border-top-0 voice-mail-container">
+            <div className="icon-container d-flex align-items-center">
+              <div className="red-icon "></div>
+              <div className="voice-icon "></div>
+            </div>
+            <div className="info-container me-3 m-auto">
+              <div className="d-flex w-100">
+                <div className="email-name w-50">Timothee</div>
+                <div className="voice-date w-50 text-end">12/30</div>
               </div>
-              <div className="d-flex justify-content-center">
-                <div
-                  className="play-icon"
-                  type="button"
-                  onClick={OpenMessage}
-                ></div>
-                <div className="sound-icon mt-1"></div>
+              <div className="d-flex w-100 align-items-center">
+                <div className="play-icon" type="button" onClick={OpenMessage}></div>
+                <div className="sound-icon"></div>
               </div>
             </div>
           </div>
 
-          <div className=" d-flex justify-content-evenly mx-1 me-3 border border-dark py-2 g-5 border-top-0">
-            <div className="unred-icon "></div>
-            <div className="voice-icon "></div>
-            <div className="d-flex align-items-start flex-column  justify-content-evenly">
-              <div className="d-flex justify-content-center">
-                <div className="email-name">Timothee</div>
-                <div className="ms-5">12/30</div>
+          <div className=" d-flex border border-dark py-0 border-top-0 voice-mail-container">
+            <div className="icon-container d-flex align-items-center">
+              <div className="unred-icon "></div>
+              <div className="voice-icon "></div>
+            </div>
+            <div className="info-container me-3 m-auto">
+              <div className="d-flex w-100">
+                <div className="email-name w-50">Timothee</div>
+                <div className="voice-date w-50 text-end">12/30</div>
               </div>
-              <div className="d-flex justify-content-center">
+              <div className="d-flex w-100 align-items-center">
+                <div className="play-icon" type="button"></div>
+                <div className="sound-icon"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className=" d-flex border border-dark py-0 border-top-0 voice-mail-container">
+            <div className="icon-container d-flex align-items-center">
+              <div className="unred-icon "></div>
+              <div className="voice-icon "></div>
+            </div>
+            <div className="info-container me-3 m-auto">
+              <div className="d-flex w-100">
+                <div className="email-name w-50">Timothee</div>
+                <div className="voice-date w-50 text-end">12/30</div>
+              </div>
+              <div className="d-flex w-100 align-items-center">
                 <div className="play-icon" type="button"></div>
                 <div className="sound-icon mt-1"></div>
               </div>
             </div>
           </div>
 
-          <div className=" d-flex justify-content-evenly mx-1 me-3 border border-dark py-2 g-5 border-top-0">
-            <div className="unred-icon "></div>
-            <div className="voice-icon "></div>
-            <div className="d-flex align-items-start flex-column  justify-content-evenly">
-              <div className="d-flex justify-content-center">
-                <div className="email-name">Timothee</div>
-                <div className="ms-5">12/30</div>
+          <div className=" d-flex border border-dark py-0 border-top-0 voice-mail-container">
+            <div className="icon-container d-flex align-items-center">
+              <div className="unred-icon "></div>
+              <div className="voice-icon "></div>
+            </div>
+            <div className="info-container me-3 m-auto">
+              <div className="d-flex w-100">
+                <div className="email-name w-50">Timothee</div>
+                <div className="voice-date w-50 text-end">12/30</div>
               </div>
-              <div className="d-flex justify-content-center">
+              <div className="d-flex w-100 align-items-center">
                 <div className="play-icon" type="button"></div>
                 <div className="sound-icon mt-1"></div>
               </div>
             </div>
           </div>
 
-          <div className=" d-flex justify-content-evenly mx-1 me-3 border border-dark py-2 g-5 border-top-0">
-            <div className="unred-icon "></div>
-            <div className="voice-icon "></div>
-            <div className="d-flex align-items-start flex-column  justify-content-evenly">
-              <div className="d-flex justify-content-center">
-                <div className="email-name">Timothee</div>
-                <div className="ms-5">12/30</div>
-              </div>
-              <div className="d-flex justify-content-center">
-                <div className="play-icon" type="button"></div>
-                <div className="sound-icon mt-1"></div>
-              </div>
+          <div className=" d-flex border border-dark py-0 border-top-0 voice-mail-container">
+            <div className="icon-container d-flex align-items-center">
+              <div className="unred-icon "></div>
+              <div className="voice-icon "></div>
             </div>
-          </div>
-
-          <div className=" d-flex justify-content-evenly mx-1 me-3 border border-dark py-2 g-5 border-top-0">
-            <div className="unred-icon "></div>
-            <div className="voice-icon "></div>
-            <div className="d-flex align-items-start flex-column justify-content-evenly">
-              <div className="d-flex justify-content-center">
-                <div className="email-name">Timothee</div>
-                <div className="ms-5">12/30</div>
+            <div className="info-container me-3 m-auto">
+              <div className="d-flex w-100">
+                <div className="email-name w-50">Timothee</div>
+                <div className="voice-date w-50 text-end">12/30</div>
               </div>
-              <div className="d-flex justify-content-center">
+              <div className="d-flex w-100 align-items-center">
                 <div className="play-icon" type="button"></div>
                 <div className="sound-icon mt-1"></div>
               </div>
