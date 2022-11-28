a548d109e23e94cf14ba499f27f5ec676db7491f
Louie Glenn Dalida <louieglenndalida@gmail.com>
2022-11-28 17:05:55 +0800

    fixed resizing chat UI

--- src/app/pages/Screen/Components/Headers/Header.jsx
+++ src/app/pages/Screen/Components/Headers/Header.jsx
@@ -2,15 +2,15 @@ import "./Header.scss";
 import { useState } from "react";
 export default function Header(props) {
   return (
-    <div id="mail-screen-one">
-      <div className=" screen-dash d-flex justify-content-between mb-3">
-        <div className="email px-3">
-          <div className="text-email">{props.name}</div>
-        </div>
-        <div className="logo-container mx-2 ">
-          <div className="logo"></div>
+      <div id="mail-screen-one">
+        <div className=" screen-dash d-flex justify-content-between align-items-center">
+          <div className="email px-3">
+            <p className="text-email">{props.name}</p>
+          </div>
+          <div className="logo-container">
+            <div className="logo"></div>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
