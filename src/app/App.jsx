4cdebf6a355879af6e472dadae4bbf62df2e2937
comandante2000 <joeycomandante19@gmail.com>
2022-12-15 11:29:58 +0800

    video added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,17 +6,19 @@ import Device from "./pages/Device/Device";
 import IntroVideo from "./pages/Screen/IntroVideo/IntroVideo";
 import { useState, useEffect } from "react";
 
-
 function App() {
-  
-  const [newuser,setNewuser]= useState (true);
+  const [newuser, setNewuser] = useState(true);
 
   return (
     <div className="container-fluid app d-flex justify-content-center">
       <Routes>
-        <Route path="/" element={<Device />} />
+        <Route
+          path="/"
+          element={
+            newuser ? <IntroVideo setNewuser={setNewuser} /> : <Device />
+          }
+        />
       </Routes>
-     
     </div>
   );
 }
