c329dfa7aefff3579d8ce66a1c30bc0c9cdc34fa
Louie Glenn Dalida <louieglenndalida@gmail.com>
2022-12-22 14:40:28 +0800

    Background FadeIn and FadeOut animations and lighthen some images

--- src/app/pages/Device/Device.jsx
+++ src/app/pages/Device/Device.jsx
@@ -9,7 +9,7 @@ import MailPage from "../Intro/Mail/Mail";
 import VoiceMailPage from "../Intro/VoiceMail/VoiceMailPage";
 import MapPage from "../Intro/Map/Map";
 import Cookies from 'js-cookie';
-export default function Device() {
+export default function Device({newuser}) {
   const [screen, showScreen] = useState("HomeScreen");
   const [press, setpress] = useState(0); //to make the button unclikable must click next
   const [episodeone, setEpisodeOne] = useState("notallowed");
@@ -31,7 +31,7 @@ export default function Device() {
     });
   return (
     <div id="pda-device">
-      <div className="container v2 d-flex justify-content-center gx-0 gy-0">
+      <div className={`container v2 d-flex justify-content-center gx-0 gy-0 ${!newuser ? 'deviceopacity' : ""}`}>
         <div className="box">
           <Header />
           {screen === "HomeScreen" && (
