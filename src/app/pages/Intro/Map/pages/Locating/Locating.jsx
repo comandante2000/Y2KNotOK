cac806dc4632650e656f3b315fcb17239115b368
Louie Glenn Dalida <louieglenndalida@gmail.com>
2022-12-26 14:00:50 +0800

    Map locating outdated updated and CDMA Screens font not style for chrome/iphone done

--- src/app/pages/Intro/Map/pages/Locating/Locating.jsx
+++ src/app/pages/Intro/Map/pages/Locating/Locating.jsx
@@ -4,7 +4,7 @@ export default function MapLocatingPage() {
     return (
       <div id="map-locating-screen">
             <div className= "Locating-map d-flex justify-content-center align-items-center" >
-              <div className= "warning d-flex align-items-center justify-content-center">Locating ..</div>
+              <div className= "warning d-flex align-items-center justify-content-center">Locating...</div>
             </div>
       </div>
     );
